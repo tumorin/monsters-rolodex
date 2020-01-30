7a9b42efcdc68e6cb09f5d6f8215dba1554ebc13
Yurii Tumorin <ytumorin@n-ix.com>
2020-01-30 15:03:21 +0200

    Updates

---
+++ precache-manifest.8309c085e39b3d4967a4cf129dcdbcfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2387868d00b5f07ecbb680fadb7d2836",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "773bcb991a5c786d0c75",
+    "url": "/github.io/monsters-rolodex/static/css/main.efcf8bb1.chunk.css"
+  },
+  {
+    "revision": "a8b76958ccd5d6d253a3",
+    "url": "/github.io/monsters-rolodex/static/js/2.7029a57e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/monsters-rolodex/static/js/2.7029a57e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "773bcb991a5c786d0c75",
+    "url": "/github.io/monsters-rolodex/static/js/main.a9303ef5.chunk.js"
+  },
+  {
+    "revision": "b387e1d403fb1a6d9304",
+    "url": "/github.io/monsters-rolodex/static/js/runtime-main.3ec96105.js"
+  }
+]);
